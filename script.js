a8ae92b1e46ee82cfe96501aa6fbc59e02149de5
Damian <assistancemom@gmail.com>
2017-09-13 17:55:12 -0600

    ok

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 var trail=[];
 var hu=0;
-const huchange=1;
+const huchange=0.5;
 
 function setup(){
   createCanvas(displayWidth, displayHeight);
   noStroke();
   colorMode(HSB);
-}
+  }
+
 
 function draw(){
-  background(0, 0, 2);
+  background(66, 66, 0);
   beginShape(TRIANGLE_STRIP);
   for (var i = 0; i < trail.length; i++) {
-    // trail[i].vel.add(p5.Vector.random2D().mult(0.5));
-    // trail[i].vel.mult(0.95);
+   // trail[i].vel.add(p5.Vector.random2D().mult(0.5));
+    trail[i].vel.mult(0.95);
     trail[i].pos.add(trail[i].vel);
     fill(map(sin((trail[i].hu)*0.01%360), -1, 1, 180, 360), 100, 100, 0.5);
     vertex(trail[i].pos.x, trail[i].pos.y);
   }
   endShape();
-  if (trail.length>100){
-    trail.splice(0, 2);
+  if (trail.length>1000){
+    trail.splice(0, 6);
   }
 }
 
